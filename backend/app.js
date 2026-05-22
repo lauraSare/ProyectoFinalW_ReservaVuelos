d9cb1f586be4bc5599713cafd0c8a8ec345d91e3
Laura Sanchez <laura.sr@tecjerez.edu.mx>
2026-05-22 13:29:44 -0600

    cors con variable de entorno

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.use(helmet());
 app.use(noAccesoDirectorios);
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization", "x-csrf-token"],
