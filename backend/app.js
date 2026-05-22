f6500eb04926e97fed293ab638e79bad524ecc4e
Laura Sanchez <laura.sr@tecjerez.edu.mx>
2026-05-22 15:02:19 -0600

    CORS y URL backend produccion

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,9 @@ app.use(helmet());
 app.use(noAccesoDirectorios);
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL 
+  ? [process.env.FRONTEND_URL, "http://localhost:5173"] 
+  : "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization", "x-csrf-token"],
